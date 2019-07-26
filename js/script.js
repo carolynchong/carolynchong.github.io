dc83c8450b6815e2cc12d4b3e6827bf68e628d00
Carolyn Chong <carolynchong15@gmail.com>
2019-07-26 16:19:09 -0400

    Fixes

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ gtag('js', new Date());
 gtag('config', 'UA-118371833-1');
 
 // Eliminate funky background jump on mobile upon scroll due to URL bar. More info here: https://stackoverflow.com/questions/24944925/background-image-jumps-when-address-bar-hides-ios-android-mobile-chrome
-var bg = $(".content");
+// var bg = $(".content");
 
-function resizeBackground() {
-    bg.height( $(window).height() + 60);
-}
+// function resizeBackground() {
+//     bg.height( $(window).height() + 60);
+// }
 
-$(window).resize(resizeBackground);
-resizeBackground();
+// $(window).resize(resizeBackground);
+// resizeBackground();
